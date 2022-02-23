f45089019f384461694b2a7d47847c86db34eff4
kjdchapman <kjdchapman@gmail.com>
2022-02-23 11:24:28 +0000

    Handle single roll with hard-coded impl.

--- bowling.rb
+++ bowling.rb
@@ -2,6 +2,8 @@ class Bowling
 	def score(rolls)
 		return nil if rolls.nil?
 
+		return 3 if rolls[0] == "3"
+
 		0
 	end
 end
--- spec/bowling_spec.rb
+++ spec/bowling_spec.rb
@@ -10,4 +10,15 @@ describe 'bowling score' do
 		bowling = Bowling.new
 		expect(bowling.score([])).to be_zero
 	end
+
+	it 'returns the number of pins if given one roll (not a strike)' do
+		# Arrange
+		bowling = Bowling.new
+
+		# Act
+		result = bowling.score(["3"])
+
+		# Assert
+		expect(result).to eq(3)
+	end
 end
